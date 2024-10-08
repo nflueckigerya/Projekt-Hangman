ede6ae2e852dd9cfb27bbdf6491c6681d4d84141
Yanik Flückiger <yanik.flueckiger@ict-campus.net>
2024-10-08 08:05:06 +0200

    final bugged version

--- hangman_advanced.py
+++ hangman_advanced.py
@@ -120,13 +120,53 @@ secretWords = [
     "Kernel"
 ]
 
-# wählt ein random Wort von der Liste
-def choseWord():
-  secretWord = secretWords[random.randint(0,59)]
-  secretWord = list(secretWord)
-  print(secretWord)
-  # erstellt die userguess liste
-  userGuess = ("_ ") * len(secretWord)
-  print(userGuess)
 
-choseWord()
+def main():
+  # anzahl versuche
+  life = 0
+  # wählt ein random Wort von der Liste
+  secretWord = secretWords[random.randint(0,59)]
+  secretWord = secretWord.upper()
+  secretWord = list(secretWord)
+  # erstellt die userguess Liste
+  userGuess = ("_ ") * len(secretWord)
+  #Liste mit den bereits falsch geratenen Buchstaben
+  alreadyGuessed = []
+  # überprüft ob das Wort noch nicht erraten ist
+  while userGuess.replace(" ", "") != secretWord:
+    print(HANGMANPICS[0] + "\n")
+    print(userGuess)
+    # fragt nach Buchstabe
+    guess = input("Guess a letter:\n")
+    guess = guess.upper()
+    # überprüft ob der guess den richtigen Datentyp hat und nicht ein bereits geratener Buchstabe ist
+    while guess in alreadyGuessed:
+      print("You already tried this letter or your input is invalid")
+      guess = input("Guess a letter:\n")
+      guess = guess.upper()
+    alreadyGuessed.append(guess)
+    # überprüft ob guess richtig ist
+    if guess in secretWord:
+      # fügt guess dem userGuess guess hinzu und zeigt dann die Überschrift
+      positionGuess = secretWord.index(guess)
+      userGuess = list(userGuess)
+      userGuess = userGuess.insert(positionGuess*2, guess)
+      userGuess = "".join(userGuess)
+      print(HANGMANPICS[life] + "\n")
+      print(userGuess + "\n")
+      print("Already guessed letters:\n" + " ".join(alreadyGuessed))
+    else: 
+      # printed das entsprechende Menü + überprüft ob noch Versuche vorhanden sind
+      life = life + 1
+      print(HANGMANPICS[life])
+      print("Already guessed letters:\n" + " ".join(alreadyGuessed))
+      if life == 6:
+        print("YOU LOST!")
+        break
+  # winscreen
+  print("YOU WON!")
+
+    
+
+
+main()
